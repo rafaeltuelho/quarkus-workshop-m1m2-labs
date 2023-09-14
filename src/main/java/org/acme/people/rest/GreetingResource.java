5feeaefa2b29af5db34e2cdc42b287d0e890f6f3
Rafael T. C. Soares (A.K.A Tuelho) <null>
2023-09-14 17:35:45 +0000

    adding monitoring resources

--- src/main/java/org/acme/people/rest/GreetingResource.java
+++ src/main/java/org/acme/people/rest/GreetingResource.java
@@ -8,17 +8,24 @@ import javax.ws.rs.core.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import io.smallrye.common.annotation.NonBlocking;
 
 @Path("/hello")
 public class GreetingResource {
 
     public static final Logger log = LoggerFactory.getLogger(GreetingResource.class);
+    private final MeterRegistry registry;
 
+    GreetingResource(MeterRegistry registry) {
+        this.registry = registry;
+    }
+    
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @NonBlocking
     public String hello() {
+        registry.counter("greeting.hello.counter").increment();
         return "hello";
     }
 }
--- src/main/java/org/acme/people/stream/NameConverter.java
+++ src/main/java/org/acme/people/stream/NameConverter.java
@@ -1,25 +1,32 @@
 package org.acme.people.stream;
 
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 import javax.enterprise.context.ApplicationScoped;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import io.smallrye.reactive.messaging.annotations.Broadcast;
 
 @ApplicationScoped
 public class NameConverter {
     
     public static final String[] honorifics = {"Mr.", "Mrs.", "Sir", "Madam", "Lord", "Lady", "Dr.", "Professor", "Vice-Chancellor", "Regent", "Provost", "Prefect"};
+    private final MeterRegistry registry;
 
+    NameConverter(MeterRegistry registry) {
+        this.registry = registry;
+    }
 
     @Incoming("names")
     @Outgoing("my-data-stream")
     @Broadcast
     public String process(String name) {
-        String honorific = honorifics[ThreadLocalRandom.current().nextInt(honorifics.length)];
+        String honorific = honorifics[(int)Math.floor(Math.random() * honorifics.length)];
+        registry.counter("nameconvert.process.counter").increment(); 
+        registry.timer("nameconvert.process.timer").record(3000, TimeUnit.MILLISECONDS); 
         return honorific + " " + name;
     }
 }
